8987aee35fbcf8892a69c06a9dd1b2b21dce99a4
warnerwarner <tom.p.a.warner@gmail.com>
2019-06-19 14:20:49 +0100

    add s to make class

--- Models/PBWidgets.py
+++ Models/PBWidgets.py
@@ -193,7 +193,7 @@ class AntiPlumeValveWidget(QtWidgets.QWidget, plumeValveDesign.Ui_Form):
 
 class BinaryPlumeValveWidget(QtWidgets.QWidget, binaryValveDesign.Ui_Form):
     def __init__(self, parentUi=None):
-        super(self.__clas__, self).__init__()
+        super(self.__class__, self).__init__()
         self.setupUi(self)
 
         self.parentUi = parentUi
