5927421d37a68f2978f9af008f9edc3655da40fa
Tom <tom.p.a.warner@gmail.com>
2019-07-04 14:53:58 +0100

    add paths

--- Controllers/main.py
+++ Controllers/main.py
@@ -1,5 +1,6 @@
 import sys
-
+sys.path.append('C:\\Users\\warnert\\Documents\\GitHub')
+sys.path.append('C:\\Users\\warnert\\Documents\\GitHub\\PulseBoy_old')
 from PyPulse import PulseInterface
 import numpy as np
 from PyQt5 import QtWidgets
